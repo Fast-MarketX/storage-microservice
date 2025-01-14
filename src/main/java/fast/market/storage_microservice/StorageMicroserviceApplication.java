11273f7aa1ec4db4b518ee83ef415b3a2c08ffdb
OlAd46kA <klim9210307@gmail.com>
2025-01-14 16:26:43 +0100

    starting repo

---
+++ src/main/java/fast/market/storage_microservice/StorageMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fast.market.storage_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fast/market/storage_microservice/StorageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.market.storage_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
