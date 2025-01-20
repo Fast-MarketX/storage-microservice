5405fc30ff2eddc58179e6970fc46f776e610ae6
OlAd46kA <klim9210307@gmail.com>
2025-01-20 18:44:03 +0100

    still trying to implement EDA

--- src/main/java/fast/market/storage_microservice/StorageMicroserviceApplication.java
+++ src/main/java/fast/market/storage_microservice/StorageMicroserviceApplication.java
@@ -2,8 +2,10 @@ package fast.market.storage_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class StorageMicroserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/fast/market/storage_microservice/config/KafkaConfig.java
+++ src/main/java/fast/market/storage_microservice/config/KafkaConfig.java
@@ -1,15 +1,16 @@
 package fast.market.storage_microservice.config;
 
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @EnableKafka
@@ -17,16 +18,17 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public KafkaTemplate<String, Map<Long, Integer>> kafkaTemplate(ProducerFactory<String, Map<Long,Integer>> producerFactory){
-        return new KafkaTemplate<>(producerFactory);
+    public NewTopic productsForStorage(){
+        return new NewTopic("products-for-storage", 1, (short) 1);
     }
 
+
     @Bean
-    public ProducerFactory<String, Map<Long, Integer>> producerFactory(){
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+    public ProducerFactory<String, List<Long>> producerFactory(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
     }
 }
